6397ab4558a85ec2042afb38df10cd00430e7d20
chinki <addityekadam@gmail.com>
2023-04-26 18:43:06 +0530

    Uploading code

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import image from '../../images/pnf.jpg'
+import './PageNotFound.scss'
+function PageNotFound() {
+  return <div className='pnf'>
+<img src={image}></img>
+
+  </div>;
+}
+
+export default PageNotFound;
